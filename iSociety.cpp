f7a99a7ff329f6ed72d90855cfd847b5e1f4a2ea
SaanviKumar13 <113087236+SaanviKumar13@users.noreply.github.com>
2023-05-02 17:56:27 +0530

    Added more functions

--- iSociety.cpp
+++ iSociety.cpp
@@ -1,14 +1,87 @@
 #include<bits/stdc++.h>
 #include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
 using namespace std;
 
+class Resident
+{
+    public:
+    long int aadharno, phno;
+    string email, name;
+    int flat_number;
+    string user_password;
+
+    void residentLogin()
+    {
+        int choice;
+        cout<<"============================================================== Resident LogIn ============================================================="<<endl;
+        cout<<"1. Show Personal Details "<<endl;
+        cout<<"2. Edit Personal Details "<<endl;
+        cout<<"3. Bills "<<endl;
+        cin>>choice;
+        switch(choice)
+        {
+            case 1: displayInfo();
+                    break;
+            case 2: acceptUserInfo(1);
+                    break;
+            case 3: bills();
+                    break;
+        }
+    }
+    void bills()
+    {
+        //display bills thru info frm admin
+
+    }
+
+    void acceptUserInfo(int n)
+    {
+        cout<<"\x1b[1mENTER YOUR DETAILS\x1b[0m"<<endl;
+        cout<<"Name:";
+        getline(cin, name);
+        cout<<"\nEmail Address: ";
+        getline(cin,email);
+        cout<<"\nPhone Number: ";
+        cin>>phno;
+        cout<<"\nAadhar Number: ";
+        cin>>aadharno;
+        if(n==0)
+        {
+            cout<<"\n Create Password: ";
+            getline(cin,user_password);
+        }
+        else if(n==1)
+        {
+            password:
+            string pass;
+            cout<<"Enter password to save changes.";
+            cin>>pass;
+            if(pass.compare(user_password)!=0)
+            {
+                cout<<"Wrong password.";
+                goto password;
+            }
+            else
+            {
+                cout<<"Saved Successfully!";
+            }
+        }
+    }
+
+    void displayInfo()
+    {
+        cout<<"Name: "<<name<<endl;
+        cout<<"Phone Number: "<<phno<<endl;
+        cout<<"E-Mail: "<<email<<endl;
+        cout<<"Aadhar Number: "<<aadharno<<endl; 
+    }
+
+};
 class Society
 {
-    private:
-        string name,address,city,state,type;
-        int pincode, phno, price, price_range, user_pin;
-        vector <string> type;
-
     public:
         void welcome()
         {
@@ -26,54 +99,97 @@ class Society
             cout<<"============================================================================================================================================================================="<<endl;
         }
 
-        void acceptUserInfo()
+        void adminLogin()
         {
-            cout<<"\x1b[1mENTER YOUR DETAILS\x1b[0m"<<endl;
-            cout<<"Name:";
-            getline(cin, name);
-            cout<<"\nAddress: ";
-            getline(cin,address);
-            cout<<"\nCity: ";
-            getline(cin,city);
-            cout<<"\nState: ";
-            getline(cin,state);
-            cout<<"\nPhone Number: ";
-            cin>>phno;
-            cout<<"\nPincode: ";
-            cin>>pincode;
+            cout<<"================================================================ Admin LogIn =============================================================="<<endl;
+            password:
+            cout<<" Enter Password: ";
+            cin>>admin_password;
+            if(admin_password !=0000)
+            {
+                cout<<"Wrong password.";
+                goto password;
+            }
+            else
+            {
+                int n;
+                cout<<""<<endl;
+                cout<<"1. Display Resident List"<<endl;
+                cout<<"2. Update residents "<<endl;
+                cout<<"3. Add bills for the residents "<<endl;
+                cout<<"4. Exit"<<endl;
+                cout<<"Enter your choice: ";
+                cin>>n;
+                switch(n)
+                {
+                    case 1: dispResidents();
+                            break;
+                    case 2: UpdateResidents();
+                            break;
+                    case 3: addBills();
+                            break;
+                    case 4: cout<<"Thank you!";
+                }   
+            }
+
+            
         }
 
-        void searchQuery()
-        {
-            cout<<"Let's get started on finding you an apartment. Depending on your preferences we will search high and low and find the perfect home for you!"<<endl;
-            cout<<"Type:";
-            getline(cin, type);
-            cout<<"Pincode of the area: ";
-            cin>>user_pin;
-            cout<<"Price range (Upper Limit): ";
-            cin>>price_range;
-        }
 
-        void search()
-        {
-            //search algo to mach apartment(from db)
-            //can return a vector of potential apartments
-        }
 
-        void results()
-        {
-            //to display the search results
-        }
+
+
+
+
+        // void searchQuery()
+        // {
+        //     cout<<"Let's get started on finding you an apartment. Depending on your preferences we will search high and low and find the perfect home for you!"<<endl;
+        //     cout<<"Type:";
+        //     getline(cin, type);
+        //     cout<<"Pincode of the area: ";
+        //     cin>>user_pin;
+        //     cout<<"Price range (Upper Limit): ";
+        //     cin>>price_range;
+        // }
+
+
+
+        // void search()
+        // {
+        //     //search algo to mach apartment(from db)
+        //     //can return a vector of potential apartments
+        // }
+
+        // void results()
+        // {
+        //     //to display the search results
+        // }
 };
 
 int main()
 {
+    int choice;
     Society s1;
+    Resident r1;
     s1.welcome(); 
     cout<<"";
-    s1.acceptUserInfo();
-    s1.searchQuery();
-    // s1.search();
-    // s1.results();
+    cout<<"================================================================= MAIN MENU ==============================================================="<<endl;
+    cout<<"1. Resident Login "<<endl;
+    cout<<"2. New User SignUp "<<endl;
+    cout<<"3. Admin Login "<<endl;
+    cout<<"4. Exit "<<endl;
+    cout<<"Enter your choice.";
+    cin>>choice;
+    switch(choice)
+    {
+        case 1: r1.residentLogin();
+                break;
+        case 2: r1.acceptUserInfo(0);
+                break;
+        case 3: s1.adminLogin();
+                break;
+        case 4: exit(0);
+                break;
+    }
     return 0;
 };
